403dc1c02a838cc15463d8635fe9119b524dad29
Sobayo <sobayotoby@gmail.com>
2023-10-01 21:23:42 +0100

    created on-page assistant

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Buttons from './components/Buttons'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Buttons />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Buttons = () => {
+  return (
+    <div className="buttons">
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+      <button>Button 4</button>
+      <button>Button 5</button>
+      <button>Button 6</button>
+    </div>
+  );
+};
+
+export default Buttons;
